273d2b146a03c915c71327bd95005d9d98762fca
lucas homer <lucas.homer@gmail.com>
2025-12-06 15:32:04 -0800

    03.03

--- exercises/03-agents/03.03-showing-tools-in-the-frontend/problem/client/root.tsx
+++ exercises/03-agents/03.03-showing-tools-in-the-frontend/problem/client/root.tsx
@@ -1,40 +1,35 @@
-import { useChat } from '@ai-sdk/react';
-import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ChatInput, Message, Wrapper } from './components.tsx';
-import './tailwind.css';
+import { useChat } from "@ai-sdk/react";
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { ChatInput, Message, Wrapper } from "./components.tsx";
+import "./tailwind.css";
+import type { MyUIMessage } from "../api/chat.ts";
 
 const App = () => {
-  // TODO - pass MyUIMessage as a type argument to useChat
-  const { messages, sendMessage } = useChat({});
+	// TODO - pass MyUIMessage as a type argument to useChat
+	const { messages, sendMessage } = useChat<MyUIMessage>({});
 
-  const [input, setInput] = useState(
-    'Tell me what todo items I have today.',
-  );
+	const [input, setInput] = useState("Tell me what todo items I have today.");
 
-  return (
-    <Wrapper>
-      {messages.map((message) => (
-        <Message
-          key={message.id}
-          role={message.role}
-          parts={message.parts}
-        />
-      ))}
-      <ChatInput
-        input={input}
-        onChange={(e) => setInput(e.target.value)}
-        onSubmit={(e) => {
-          e.preventDefault();
-          sendMessage({
-            text: input,
-          });
-          setInput('');
-        }}
-      />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			{messages.map((message) => (
+				<Message key={message.id} role={message.role} parts={message.parts} />
+			))}
+			<ChatInput
+				input={input}
+				onChange={(e) => setInput(e.target.value)}
+				onSubmit={(e) => {
+					e.preventDefault();
+					sendMessage({
+						text: input,
+					});
+					setInput("");
+				}}
+			/>
+		</Wrapper>
+	);
 };
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
