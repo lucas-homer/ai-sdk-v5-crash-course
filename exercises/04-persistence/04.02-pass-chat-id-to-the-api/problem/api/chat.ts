1e3cc440a5fae5323998de9862d2fa971177da17
lucas homer <lucas.homer@gmail.com>
2025-12-06 17:28:51 -0800

    through 04.02

--- exercises/04-persistence/04.02-pass-chat-id-to-the-api/problem/api/chat.ts
+++ exercises/04-persistence/04.02-pass-chat-id-to-the-api/problem/api/chat.ts
@@ -1,21 +1,16 @@
-import {
-  convertToModelMessages,
-  streamText,
-  type UIMessage,
-} from 'ai';
-import { google } from '@ai-sdk/google';
+import { google } from "@ai-sdk/google";
+import { convertToModelMessages, streamText, type UIMessage } from "ai";
 
 export const POST = async (req: Request): Promise<Response> => {
-  const body: { messages: UIMessage[]; id: string } =
-    await req.json();
-  const { messages, id } = body;
+	const body: { messages: UIMessage[]; id: string } = await req.json();
+	const { messages, id } = body;
 
-  console.log('id', id);
+	console.log("id", id);
 
-  const result = streamText({
-    model: google('gemini-2.0-flash-001'),
-    messages: convertToModelMessages(messages),
-  });
+	const result = streamText({
+		model: google("gemini-2.5-flash"),
+		messages: convertToModelMessages(messages),
+	});
 
-  return result.toUIMessageStreamResponse();
+	return result.toUIMessageStreamResponse();
 };
