125c9820a32a73e826727544c4efb88affb21381
lucas homer <lucas.homer@gmail.com>
2025-12-06 16:06:13 -0800

    through 03.05

--- exercises/03-agents/03.04-mcp-via-stdio/problem/api/chat.ts
+++ exercises/03-agents/03.04-mcp-via-stdio/problem/api/chat.ts
@@ -1,40 +1,55 @@
-import { google } from '@ai-sdk/google';
+import { google } from "@ai-sdk/google";
 import {
-  convertToModelMessages,
-  stepCountIs,
-  streamText,
-  type UIMessage,
-} from 'ai';
-import { experimental_createMCPClient as createMCPClient } from 'ai';
-import { Experimental_StdioMCPTransport as StdioMCPTransport } from 'ai/mcp-stdio';
+	convertToModelMessages,
+	experimental_createMCPClient as createMCPClient,
+	stepCountIs,
+	streamText,
+	type UIMessage,
+} from "ai";
+import { Experimental_StdioMCPTransport as StdioMCPTransport } from "ai/mcp-stdio";
 
 if (!process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
-  throw new Error('GITHUB_PERSONAL_ACCESS_TOKEN is not set');
+	throw new Error("GITHUB_PERSONAL_ACCESS_TOKEN is not set");
 }
 
+const myTransport = new StdioMCPTransport({
+	command: "docker",
+	args: [
+		"run",
+		"-i",
+		"--rm",
+		"-e",
+		"GITHUB_PERSONAL_ACCESS_TOKEN",
+		"ghcr.io/github/github-mcp-server",
+	],
+	env: {
+		GITHUB_PERSONAL_ACCESS_TOKEN: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
+	},
+});
+
 export const POST = async (req: Request): Promise<Response> => {
-  const body: { messages: UIMessage[] } = await req.json();
-  const { messages } = body;
+	const body: { messages: UIMessage[] } = await req.json();
+	const { messages } = body;
 
-  // TODO - create an MCP client that uses the StdioMCPTransport
-  // to connect to the GitHub MCP server
-  const mcpClient = TODO;
+	// TODO - create an MCP client that uses the StdioMCPTransport
+	// to connect to the GitHub MCP server
+	const mcpClient = await createMCPClient({ transport: myTransport });
 
-  const result = streamText({
-    model: google('gemini-2.5-flash'),
-    messages: convertToModelMessages(messages),
-    system: `
+	const result = streamText({
+		model: google("gemini-2.5-flash"),
+		messages: convertToModelMessages(messages),
+		system: `
       You are a helpful assistant that can use the GitHub API to interact with the user's GitHub account.
     `,
-    // TODO - use the mcpClient.tools() method to get the tools
-    tools: TODO,
-    stopWhen: [stepCountIs(10)],
-  });
+		// TODO - use the mcpClient.tools() method to get the tools
+		tools: await mcpClient.tools(),
+		stopWhen: [stepCountIs(10)],
+	});
 
-  return result.toUIMessageStreamResponse({
-    // TODO - use the mcpClient.close() method to close the MCP client
-    // when the stream is finished. This will also close the process
-    // running the GitHub MCP server.
-    onFinish: TODO,
-  });
+	return result.toUIMessageStreamResponse({
+		// TODO - use the mcpClient.close() method to close the MCP client
+		// when the stream is finished. This will also close the process
+		// running the GitHub MCP server.
+		onFinish: async () => await mcpClient.close(),
+	});
 };
