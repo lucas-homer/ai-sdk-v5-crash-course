796fff36a0bc3155b1e49d3fae6885b0c28b8a8d
lucas homer <lucas.homer@gmail.com>
2025-12-10 08:47:15 -0800

    through 07.01

--- exercises/07-streaming/07.01-custom-data-parts/problem/client/root.tsx
+++ exercises/07-streaming/07.01-custom-data-parts/problem/client/root.tsx
@@ -1,48 +1,49 @@
-import { useChat } from '@ai-sdk/react';
-import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ChatInput, Message, Wrapper } from './components.tsx';
-import './tailwind.css';
-import type { MyMessage } from '../api/chat.ts';
+import { useChat } from "@ai-sdk/react";
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { ChatInput, Message, Wrapper } from "./components.tsx";
+import "./tailwind.css";
+import type { MyMessage } from "../api/chat.ts";
 
 const App = () => {
-  const { messages, sendMessage } = useChat<MyMessage>({});
+	const { messages, sendMessage } = useChat<MyMessage>({});
 
-  const [input, setInput] = useState(``);
+	const [input, setInput] = useState(``);
 
-  // TODO: Get the data-suggestion part from the last message
-  const latestSuggestion: string | undefined = TODO;
+	// TODO: Get the data-suggestion part from the last message
+	const latestSuggestion: string | undefined =
+		messages.length === 0
+			? undefined
+			: messages[messages.length - 1]?.parts.find(
+					(part) => part.type === "data-suggestion",
+				)?.data;
 
-  return (
-    <Wrapper>
-      {messages.map((message) => (
-        <Message
-          key={message.id}
-          role={message.role}
-          parts={message.parts}
-        />
-      ))}
-      <ChatInput
-        // NOTE: We are passing the suggestion to the ChatInput component
-        // where we will display it as a button
-        suggestion={
-          messages.length === 0
-            ? 'What is the capital of France?'
-            : latestSuggestion
-        }
-        input={input}
-        onChange={(text) => setInput(text)}
-        onSubmit={(e) => {
-          e.preventDefault();
-          sendMessage({
-            text: input,
-          });
-          setInput('');
-        }}
-      />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			{messages.map((message) => (
+				<Message key={message.id} role={message.role} parts={message.parts} />
+			))}
+			<ChatInput
+				// NOTE: We are passing the suggestion to the ChatInput component
+				// where we will display it as a button
+				suggestion={
+					messages.length === 0
+						? "What is the capital of France?"
+						: latestSuggestion
+				}
+				input={input}
+				onChange={(text) => setInput(text)}
+				onSubmit={(e) => {
+					e.preventDefault();
+					sendMessage({
+						text: input,
+					});
+					setInput("");
+				}}
+			/>
+		</Wrapper>
+	);
 };
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
