a24579504b19fda82562dc5f0233b523294c3dfe
lucas homer <lucas.homer@gmail.com>
2025-12-13 15:56:53 -0800

    08.01

--- exercises/08-agents-and-workflows/08.01-workflow/problem/api/chat.ts
+++ exercises/08-agents-and-workflows/08.01-workflow/problem/api/chat.ts
@@ -1,20 +1,25 @@
-import { google } from '@ai-sdk/google';
-import { generateText, streamText, type UIMessage } from 'ai';
+import { google } from "@ai-sdk/google";
+import {
+	convertToModelMessages,
+	generateText,
+	streamText,
+	type UIMessage,
+} from "ai";
 
 const formatMessageHistory = (messages: UIMessage[]) => {
-  return messages
-    .map((message) => {
-      return `${message.role}: ${message.parts
-        .map((part) => {
-          if (part.type === 'text') {
-            return part.text;
-          }
+	return messages
+		.map((message) => {
+			return `${message.role}: ${message.parts
+				.map((part) => {
+					if (part.type === "text") {
+						return part.text;
+					}
 
-          return '';
-        })
-        .join('')}`;
-    })
-    .join('\n');
+					return "";
+				})
+				.join("")}`;
+		})
+		.join("\n");
 };
 
 const WRITE_SLACK_MESSAGE_FIRST_DRAFT_SYSTEM = `You are writing a Slack message for a user based on the conversation history. Only return the Slack message, no other text.`;
@@ -30,14 +35,41 @@ const WRITE_SLACK_MESSAGE_FINAL_SYSTEM = `You are writing a Slack message based 
 `;
 
 export const POST = async (req: Request): Promise<Response> => {
-  const body: { messages: UIMessage[] } = await req.json();
-  const { messages } = body;
+	const body: { messages: UIMessage[] } = await req.json();
+	const { messages } = body;
 
-  const writeSlackResult = TODO; // Write Slack message
+	const writeSlackResult = await generateText({
+		system: WRITE_SLACK_MESSAGE_FIRST_DRAFT_SYSTEM,
+		model: google("gemini-2.5-flash"),
+		prompt: `
+      Conversation history:
+      ${formatMessageHistory(messages)}
+    `,
+	});
 
-  const evaluateSlackResult = TODO; // Evaluate Slack message
+	const evaluateSlackResult = await generateText({
+		system: EVALUATE_SLACK_MESSAGE_SYSTEM,
+		model: google("gemini-2.5-flash"),
+		prompt: `
+      Conversation history:
+      ${formatMessageHistory(messages)}
+      Slack result:
+      ${writeSlackResult.text}
+    `,
+	});
 
-  const finalSlackAttempt = TODO; // Write final Slack message
+	const finalSlackAttempt = streamText({
+		system: WRITE_SLACK_MESSAGE_FINAL_SYSTEM,
+		model: google("gemini-2.5-flash"),
+		prompt: `
+      Conversation history:
+      ${formatMessageHistory(messages)}
+      First draft:
+      ${writeSlackResult.text}
+      Previous feedback:
+      ${evaluateSlackResult.text}
+    `,
+	});
 
-  return finalSlackAttempt.toUIMessageStreamResponse();
+	return finalSlackAttempt.toUIMessageStreamResponse();
 };
